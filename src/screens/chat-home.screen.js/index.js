a1ac24e2cbbe0719f4ed3303a8f972148c6aca2f
Ajaya1000 <ajaymati1000@gmail.com>
2022-01-10 14:22:34 +0530

    update ui on chat home

--- src/screens/chat-home.screen.js/index.js
+++ src/screens/chat-home.screen.js/index.js
@@ -2,14 +2,32 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Text, TouchableOpacity, View } from 'react-native';
 
+import { styles } from './index.style';
+
 export const ChatHome = () => {
   const navigation = useNavigation();
+
+  const chats = [
+    {
+      type: 'supplier',
+      name: 'Supplier',
+    },
+    {
+      type: 'customer',
+      name: 'Customer',
+    },
+  ];
+
   return (
     <View>
-      <Text>chat home</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('CHAT')}>
-        <Text> Click here to go to p2p chat</Text>
-      </TouchableOpacity>
+      {chats.map(item => (
+        <TouchableOpacity
+          style={styles.container}
+          key={item.name}
+          onPress={() => navigation.navigate('CHAT')}>
+          <Text style={styles.text}> {item.name}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 };
